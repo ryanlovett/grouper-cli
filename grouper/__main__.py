7d00073f7b7d98d4fe92ad91c873bddcd0ed08fe
ryanlovett <rylo@berkeley.edu>
2025-08-07 16:45:58 -0700

    Add support for `python -m grouper`.

--- grouper/__main__.py
+++ grouper/__main__.py
@@ -260,3 +260,7 @@ def main():
         except Exception as e:
             logger.error(f"Error getting subject information: {e}")
             sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
